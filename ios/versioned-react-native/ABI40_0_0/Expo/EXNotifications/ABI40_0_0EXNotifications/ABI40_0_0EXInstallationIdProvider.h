ab0a51e70e112dcfb92af05cf94cfa7a454bdd1a
Eric Samelson <eric@expo.io>
2020-11-20 17:57:22 -0800

    [ios] add ABI 40

---
+++ ios/versioned-react-native/ABI40_0_0/Expo/EXNotifications/ABI40_0_0EXNotifications/ABI40_0_0EXInstallationIdProvider.h
@@ -0,0 +1,13 @@
+// Copyright 2018-present 650 Industries. All rights reserved.
+
+#import <ABI40_0_0UMCore/ABI40_0_0UMExportedModule.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ABI40_0_0EXInstallationIdProvider : ABI40_0_0UMExportedModule
+
+- (NSString *)getInstallationId;
+
+@end
+
+NS_ASSUME_NONNULL_END
